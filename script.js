471ab0abeb83e3674ae98f6150c2ef9532ac8f7a
MUHAMMAD SUFI NARITSUL ARDHI <36657204+muhsufiardhi19@users.noreply.github.com>
2022-04-25 03:51:02 +0000

    Column Comment

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+/*
+MUHAMMAD SUFI N. ARDHI
+QBIT - Junior Programmer
+Test2
+
+<< TIPE DATA >>
+type IComment =
+{
+    commentId       : Number,
+    commentContent  : String,
+    Replies         : IComment[];
+}
+*/
+
+// Count Nested Array
+
 const comments = [1, [11, [111, 112], 12, [121]], 2];
 
 Array.prototype.getLength = function () {
